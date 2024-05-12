8cb46a3ada7fba5148247373a82428d4f3787933
sachinknsachi <sachinknsachi@gmail.com>
2024-05-12 18:26:26 +0530

    Added browser context and reporter types.

---
+++ sachin-tests/Practice/tests-3/08_Reporter.spec.js
@@ -0,0 +1,17 @@
+const {test} = require('@playwright/test');
+
+
+test('Test-1', async () => {
+})
+
+test('Test-2', async () => {
+})
+
+test('Test-3', async () => {
+})
+
+test('Test-4', async () => {
+})
+
+test('Test-5', async () => {
+})
