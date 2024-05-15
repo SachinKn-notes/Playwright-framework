49be3fd2082f95278e9d6fc013fe7713b36ec94a
sachinknsachi <sachinknsachi@gmail.com>
2024-05-15 10:30:05 +0530

    no need of fs (File System) to read json file

--- sachin-tests/Practice_02/DataDrivenTests/DDT.spec.js
+++ sachin-tests/Practice_02/DataDrivenTests/DDT.spec.js
@@ -1,7 +1,8 @@
 const {test, expect} = require('@playwright/test');
 const fs = require('fs');
 
-const testData = JSON.parse(fs.readFileSync('sachin-tests/Practice_02/DataDrivenTests/testData.json'));
+// const testData = JSON.parse(fs.readFileSync('sachin-tests/Practice_02/DataDrivenTests/testData.json'));
+const testData = require('./testData.json');
 
 test.describe('DD Test suit', () => {
     testData.forEach((node, index) => {
