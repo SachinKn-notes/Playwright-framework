83db33e956bc95aeb6b6df95f4f2c5ccf48a6c54
Sachin Kn <106311617+sachinknsachi@users.noreply.github.com>
2024-08-07 11:18:11 +0530

    Update FileUtils.js

--- libs/utils/FileUtils.js
+++ libs/utils/FileUtils.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const ini = require('ini');
 const path = require('path');
+const ini = require('ini');
+const yaml = require('js-yaml');
 
 function readFile(filePath) {
     return fs.readFileSync(path.join(process.env.ProjectPath, filePath), 'utf-8');
@@ -10,4 +11,8 @@ function readIniFile(filePath) {
     return ini.parse(readFile(filePath));
 }
 
-module.exports = { readFile, readIniFile }
+function readYamlFile(filePath) {
+    return yaml.load(readFile(filePath));
+}
+
+module.exports = { readFile, readIniFile, readYamlFile }
