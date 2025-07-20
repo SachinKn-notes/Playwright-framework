9f7a73369ab18ee170cddb8dad52eb7edbee3348
SachinKn <106311617+sachinknsachi@users.noreply.github.com>
2025-07-20 10:52:48 +0530

    added test scripts

---
+++ tests/Test-Scripts/test.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+
+test('Login test - @id-1 - @smoke', async ({page}) => {
+    console.log("Test - 1 Completed")
+})
+
+test('Login test - @id-2 - @reg', async ({page}) => {
+    console.log("Test - 2 Completed")
+})
+
+test('Login test - @id-3 - @smoke', async ({page}) => {
+    console.log("Test - 3 Completed")
+})
+
+test('Login test - @id-4 - @reg', async ({page}) => {
+    console.log("Test - 4 Completed")
+})
+
+test('Login test - @id-5 - @smoke', async ({page}) => {
+    console.log("Test - 5 Completed")
+})
+
+test('Login test - @id-6 - @reg @smoke', async ({page}) => {
+    console.log("Test - 6 Completed")
+})
