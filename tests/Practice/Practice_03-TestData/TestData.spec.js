e6dc18d0517ab7cc4a59dc898baba40cfeeadc2c
SachinKn <106311617+sachinknsachi@users.noreply.github.com>
2025-07-20 10:57:36 +0530

    correccted the path

--- tests/Practice/Practice_03-TestData/TestData.spec.js
+++ tests/Practice/Practice_03-TestData/TestData.spec.js
@@ -10,7 +10,7 @@ console.log(testData2.age);
 const fs = require('fs');
 const ini = require('ini');
 
-const config = ini.parse(fs.readFileSync('./sachin-tests/Practice/Practice_03-TestData/testData_file.ini', 'utf-8'));
+const config = ini.parse(fs.readFileSync('./tests/Practice/Practice_03-TestData/testData_file.ini', 'utf-8'));
 console.log(config.details.name, config.details.age);
 
 // .env file
